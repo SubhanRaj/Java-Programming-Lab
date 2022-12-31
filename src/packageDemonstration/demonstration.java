2fc1668f178ac48845686ce846c011994e1e7f7b
Subhan Raj <shubhanraj2002@gmail.com>
2022-12-31 18:30:57 +0530

    Demonstration of Package Creation and use

---
+++ src/packageDemonstration/demonstration.java
@@ -0,0 +1,11 @@
+// Show how to use custom package
+package packageDemonstration;
+
+import packageDemonstration.p1.ex1;
+
+public class demonstration {
+    public static void main(String[] args) {
+        ex1 ex = new ex1();
+        ex.main(args);
+    }
+}
---
+++ src/packageDemonstration/p1/ex1.java
@@ -0,0 +1,10 @@
+// Create the package packageDemonstration.p1
+package packageDemonstration.p1;
+
+public class ex1 {
+    public void main(String[] args) {
+        int num = 10;
+        num++;
+        System.out.println("Incremented number: " + num);
+    }
+}
