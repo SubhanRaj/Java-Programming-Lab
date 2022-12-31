3e5beb3296e23f03b944440472ab9cc55717cb5f
Subhan Raj <shubhanraj2002@gmail.com>
2022-12-31 16:30:24 +0530

    Java Program to display the default value of all primative data types in Java

---
+++ src/primativeDataType/primativeDataType.java
@@ -0,0 +1,40 @@
+// Java Program to display the default value of all primative data types in Java
+
+package primativeDataType;
+
+
+public class primativeDataType {
+    public static void main(String[] args) {
+        // byte
+        byte byteValue = 0;
+        System.out.println("byteValue = " + byteValue);
+
+        // short
+        short shortValue = 0;
+        System.out.println("shortValue = " + shortValue);
+
+        // int
+        int intValue = 0;
+        System.out.println("intValue = " + intValue);
+
+        // long
+        long longValue = 0;
+        System.out.println("longValue = " + longValue);
+
+        // float
+        float floatValue = 0;
+        System.out.println("floatValue = " + floatValue);
+
+        // double
+        double doubleValue = 0;
+        System.out.println("doubleValue = " + doubleValue);
+
+        // char
+        char charValue = 0;
+        System.out.println("charValue = " + charValue);
+
+        // boolean
+        boolean booleanValue = false;
+        System.out.println("booleanValue = " + booleanValue);
+    }
+}
